59e740359bb1d11eab2654ece7aae01efd21c75a
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2022-10-21 06:56:47 +0300

    Added basic proof of concept API

---
+++ TransactionL/TransactionL/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public interface ISomeInterface
+{
+    void Amogus();
+}
+
+public class SomeClass : ISomeInterface
+{
+    [Transactional]
+    public void Amogus()
+    {
+        Console.WriteLine("Amogus");
+    }
+}
+
+public class TransactionalAttribute : Attribute
+{
+    
+}
